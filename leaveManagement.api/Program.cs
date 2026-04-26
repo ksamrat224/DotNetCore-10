62f059cbaead4178076f9dc6e176498ea6d74186
Samrat <ksamrat224@gmail.com>
2026-04-26 08:24:18 +0545

    chore: add initial project files for leaveManagement.api

--- leaveManagement.api/Program.cs
+++ leaveManagement.api/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
+//creating the instance of the database context and seeding the data
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
